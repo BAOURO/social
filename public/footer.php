398af7df141856b7590fd9b35774d542aaad1840
Roi_hassan <daoudahassan@gmail.com>
2018-04-01 14:05:43 +0200

    Creation de header et footer

---
+++ public/footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Santé Quotidien</title>
+
+  <!-- CSS  -->
+  <link href="materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="materialize/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+
+<footer class="page-footer light-blue lighten-1">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Developpeurs Open-Source</h5>
+                    <p class="grey-text text-lighten-4">
+						Ce Petit utilitaire est conçu dans un but non lucratif pour aider un grand nombre des personnes.
+					Afin de connaître quelques petites astuces de premier soin en cas d'urgence. Il est a noté que cet 
+					utilitaire est un replique du logiciel Votre santé au quotidient de Larousse médical.
+					</p>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Sources</h5>
+                    <ul>
+                        <li><a class="white-text" href="#">Larousse Médical</a></li>
+                        <li><a class="white-text" href="#">Votre santé au quotidient</a></li>
+                    </ul>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Contact</h5>
+                    <p class="white-text">
+                        Emails: <br>
+                                daoudahassan@gmail.com <br>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright fixed"></div>
+    </footer>
+  <!--  Scripts-->
+  <script src="materialize/js/jquery.js"></script>
+  <script src="materialize/js/materialize.js"></script>
+  <script src="materialize/js/init.js"></script>
---
+++ public/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Social</title>
+
+  <!-- CSS  -->
+  <link href="../assets/libs/materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="../assets/libs/materialize/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+    <div class="navbar-fixed">
+        <nav class="light-blue lighten-1" role="navigation">
+            <div class="nav-wrapper container">
+				<a id="logo-container" href="#" class="brand-logo">
+					<!--img src="#" class="responsive-img circle"-->
+				</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#">ACCUEIL</a></li>
+                    <li><a href="#">MESSAGES</a></li>
+                    <li><a href="#">NOTIFICATIONS</a></li>
+                    <li><a href="#">PROFIL</a></li>
+                    <li><a href="#">DECONNEXION</a></li>
+                </ul>
+				
+                <ul id="nav-mobile" class="side-nav">
+					<!--li href="index.php"><img src="#" class="responsive-img circle center"></li-->
+					<li class="divider"></li>
+                    <li><a class="waves-effect waves-light-green" href="#"><i class="mdi-action-home left"></i>ACCUEIL</a></li>
+                    <li><a class="waves-effect waves-light-green" href="#"><i class="mdi-maps-local-hospital left"></i>MESSAGES</a></li>
+                    <li><a class="waves-effect waves-light-green" href="#"><i class="mdi-social-people left"></i>NOTIFICATION</a></li>
+                    <li><a class="waves-effect waves-light-green" href="#"><i class="mdi-action-work left"></i>PROFIL</a></li>
+                    <li><a class="waves-effect waves-light-green" href="#"><i class="mdi-action-work left"></i>DECONNEXION</a></li>
+                </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+            </div>
+        </nav>
+    </div>
+	
+	 <!--  Scripts-->
+  <script src="../assets/libs/materialize/js/jquery.js"></script>
+  <script src="../assets/libs/materialize/js/materialize.js"></script>
+  <script src="../assets/libs/materialize/js/init.js"></script>
