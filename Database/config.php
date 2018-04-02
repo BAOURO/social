83176d243b0500015a0c597c5bdaa8edcafecb26
Roi_hassan <daoudahassan@gmail.com>
2018-04-02 12:41:16 +0200

    connexion to the Database and sign up

--- Database/config.php
+++ Database/config.php
@@ -5,3 +5,20 @@
  * Date: 02/04/2018
  * Time: 10:16
  */
+
+function connexion() {
+
+    $db_host = "localhost";
+    $db_name = "social";
+    $db_user = "root";
+    $db_pass = "";
+
+    try{
+        $pdo = new PDO('mysql:host"'.$db_host.'"";"'.$db_name.'"', $db_user, $db_pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        return ($pdo);
+
+    }catch (Exception $e){
+        die($e->getMessage());
+    }
+}
---
+++ Database/inscription.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ghost
+ * Date: 02/04/2018
+ * Time: 10:43
+ */
+require_once "config.php";
+
+
+if(isset($_POST['btn-sign'])){
+    $nom = htmlspecialchars($_POST['nom']);
+    $prenom = htmlspecialchars($_POST['prenom']);
+    $date = htmlspecialchars($_POST['date']);
+    $lieu = htmlspecialchars($_POST['lieu']);
+    $sexe = htmlspecialchars($_POST['sexe']);
+    $mail = htmlspecialchars($_POST['mail']);
+    $login = htmlspecialchars($_POST['login']);
+    $pass = htmlspecialchars($_POST['pass']);
+    $cpass = htmlspecialchars($_POST['cpass']);
+    $status = 1;
+    $id_users = 0;
+
+    if($pass != $cpass){
+        echo "Les mots de passe doivent etre identiques ";
+        require "inscription.php";
+    }else{
+        $pdo = new PDO('mysql:host=localhost;dbname=social','root','');
+        $password =md5($pass);
+
+        try{
+
+            $req = $pdo->prepare('INSERT INTO users VALUES (NULL , :nom_users, :prenom_users, :date_users, :lieu_users, :sexe_users, :mail_users)');
+            $req->bindValue(":nom_users",$nom);
+            $req->bindValue(":prenom_users",$prenom);
+            $req->bindValue(":date_users",$date);
+            $req->bindValue(":lieu_users",$lieu);
+            $req->bindValue(":sexe_users",$sexe);
+            $req->bindValue(":mail_users",$mail);
+            $ok_users = $req->execute();
+            $id_users = $pdo->lastInsertId();
+
+            if($ok_users){
+                $req_log = $pdo->prepare('INSERT INTO comptes VALUES (NULL , :id_users, :login, :password, :status)');
+                $req_log->bindValue(":id_users",$id_users, PDO::PARAM_INT);
+                $req_log->bindValue(":login",$login, PDO::PARAM_STR);
+                $req_log->bindValue(":password",$password, PDO::PARAM_STR);
+                $req_log->bindValue(":status",$status, PDO::PARAM_INT);
+                $ok = $req_log->execute();
+
+                if($ok){
+                    require "../index.php";
+                }else{
+
+                    require "inscription.php";
+                }
+            }else{
+                require "inscription.php";
+            }
+        }catch (Exception $e){
+            $e->getMessage();
+        }
+    }
+
+}
