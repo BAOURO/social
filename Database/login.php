ded6643af72d6bdfde8751fc4a11bfe9f242df94
Roi_hassan <daoudahassan@gmail.com>
2018-04-05 09:21:44 +0200

    Creation de la page d'accueil

---
+++ Database/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ghost
+ * Date: 02/04/2018
+ * Time: 16:52
+ */
---
+++ public/accueil.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ghost
+ * Date: 02/04/2018
+ * Time: 17:44
+ */
+require_once "header.php";
+?>
+
+<div class="row">
+    <div class="col s12 m12 l12">
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../image/avatar.jpeg">
+                            <span class="card-title">Card Title</span>
+                        </div>
+                        <div class="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">This is a link</a>
+                            <a href='#'>This is a link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
